ea4fffce31b80e1e95a057a22f6d55781c8db590
Ryan Collins <rjosephc3@gmail.com>
2022-12-26 13:21:55 -0500

    Tweaked spacing of temporary logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div>
-	<header class="text-5xl bg-slate-400 pl-10 pt-10 pb-10">
-        <img class="inline w-96 -m-20 -ml-40 pl-10" src="book.svg" alt="" />
-        <h1 class="inline-block -ml-5">Book Band</h1>
+	<header class="text-5xl bg-slate-400 pl-10 pt-10 pb-10 flex flex-row">
+        <img class="inline w-96 -ml-24 -mt-12 -mr-32 -mb-10" src="book.svg" alt="" />
+        <h1 class="block mt-5">Book <br />Band</h1>
     </header>
 	<slot />
 </div>
