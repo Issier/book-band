51421f70259c114961e0fc6f4947bc69ce5e46d2
Ryan Collins <rjosephc3@gmail.com>
2022-12-26 13:25:37 -0500

    Centered(ish) the logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div>
-	<header class="text-5xl bg-slate-400 pl-10 pt-10 pb-10 flex flex-row">
+	<header class="text-5xl bg-slate-400 pl-10 pt-10 pb-10 flex flex-row justify-center">
         <img class="inline w-96 -ml-24 -mt-12 -mr-32 -mb-10" src="book.svg" alt="" />
         <h1 class="block mt-5">Book <br />Band</h1>
     </header>
