d667ed58c91e1de8b0f9d7f712412721a5590eae
Ryan Collins <rjosephc3@gmail.com>
2022-12-26 12:36:29 -0500

    Added server load file for book api that uses proper handling of secrets (so that I don't push up a key that actually matters next time)

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,6 @@
+import { BOOK_API_KEY } from "$env/static/private";
+
 export async function load() {
-    let book = await fetch("https://www.googleapis.com/books/v1/volumes/RX5EugEACAAJ?key={keyName}")
+    let book = await fetch(`https://www.googleapis.com/books/v1/volumes/RX5EugEACAAJ?key=${BOOK_API_KEY}`)
     return { book: book.json() }
 }
