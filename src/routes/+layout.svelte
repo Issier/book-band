b998e9d89de427c89cfb5ff15a835fe214486c75
Ryan Collins <rjosephc3@gmail.com>
2022-12-26 13:14:19 -0500

    Added temporary icon for header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
 </script>
 
 <div>
-	<header class="text-5xl bg-slate-400 text-center pt-10 pb-10">Book Band</header>
+	<header class="text-5xl bg-slate-400 pl-10 pt-10 pb-10">
+        <img class="inline w-96 -m-20 -ml-40 pl-10" src="book.svg" alt="" />
+        <h1 class="inline-block -ml-5">Book Band</h1>
+    </header>
 	<slot />
 </div>
